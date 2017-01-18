26f3cb864e83d25d2880b93049e5d45737bc637c
Architvaidya <archit.vaidya@gmail.com>
2017-01-18 16:51:30 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var middleware = require('./middleware.js');
+
+
+//app.use(middleware.requireauthentication);
+app.use(middleware.logger);
+var PORT = process.env.port || 3000;
+
+app.get('/about', middleware.requireauthentication, function(req, res){
+	res.send('About page ');
+});
+
+app.use(express.static(__dirname+'/public'));
+//console.log(__dirname); 
+//about
+// About us
+app.listen(PORT,function(){
+	console.log('Server Started on port '+PORT);
+}); 
